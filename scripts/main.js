e34de03bf11a45f598764dd758aefc05720dcf78
Russell Schoenbeck <russell.schoenbeck@me.com>
2013-11-12 21:10:33 -0600

    merged in master changes; activated layout switching buttons; added some comments in script file

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
 (function() {
+	//Replace the current style sheet with the one that was selected
 	$('footer button').click(function() {
 		var $this = $(this), 
 				stylesheet = $this.data('file'),
 				change = $this.siblings('footer button').data('file'),
 				link = $('link[href*="' + change + '"]');
 		link.attr('href', 'styles/' + stylesheet + '.css');
+		//Disable the button that was just selected, enable the opposite button
 		$this
 			.attr('disabled', 'disabled')
 			.siblings('footer button')
